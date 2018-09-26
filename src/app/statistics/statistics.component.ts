7343ee8ac8360b7fcb1490572e02dec15cebf801
Milan Vadher <milanvadher1996@gmail.com>
2018-09-26 11:23:50 +0530

    generate and display patterns in line chart

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -10,11 +10,17 @@ import { RestapiService } from '../restapi.service';
 export class StatisticsComponent implements OnInit {
 
   stock: StockChart;
+  // price: Chart;
   price: StockChart;
   groupingUnits = [['week', [1]], ['month', [1, 2, 3, 4, 6]]];
+  trianglePattern = [];
+  priceData = [];
+  stockData = [];
+  volumeData = [];
 
   constructor(public api: RestapiService) {
     this.getData();
+    this.getTrianglePattern();
   }
 
   ngOnInit() {
@@ -23,43 +29,90 @@ export class StatisticsComponent implements OnInit {
   getData() {
     this.api.getApi('/getData').subscribe(data => {
       console.log('Chart Data: ', data);
-      const priceData = [];
-      const stockData = [];
-      const volumeData = [];
       for (let index = 0; index < data[0]['date'].length; index++) {
-        priceData.push([new Date(data[0]['date'][index]).getTime(), data[4]['close'][index]]);
+        this.priceData.push([new Date(data[0]['date'][index]).getTime(), data[4]['close'][index]]);
         // tslint:disable-next-line:max-line-length
-        stockData.push([new Date(data[0]['date'][index]).getTime(), data[1]['open'][index], data[2]['high'][index], data[3]['low'][index], data[4]['close'][index]]);
-        volumeData.push([new Date(data[0]['date'][index]).getTime(), data[5]['volume'][index]]);
+        this.stockData.push([new Date(data[0]['date'][index]).getTime(), data[1]['open'][index], data[2]['high'][index], data[3]['low'][index], data[4]['close'][index]]);
+        this.volumeData.push([new Date(data[0]['date'][index]).getTime(), data[5]['volume'][index]]);
       }
-      this.setPriceGraphData(priceData, data[0]['date'].length);
-      this.setStockGraphData(stockData, volumeData, data[0]['date'].length);
+      this.api.getApi('/getTrianglePattern').subscribe((pattern: any) => {
+        console.log('Triangle Pattern: ', pattern.trianglePattern);
+        const trianglePattern = pattern.trianglePattern;
+        for (let index = 0; index < pattern.trianglePattern.length; index++) {
+          for (let i = 0; i < 3; i++) {
+            this.trianglePattern.push([new Date(trianglePattern[index][i][0]).getTime(), trianglePattern[index][i][1]]);
+          }
+        }
+        console.log(new Date(trianglePattern[25][0][0]).getTime());
+        this.setPriceGraphData(this.volumeData, this.trianglePattern, data[0]['date'].length);
+        this.setStockGraphData(this.stockData, this.volumeData, data[0]['date'].length);
+      }, (err) => {
+        console.log('Error: getTrianglePattern: ', err);
+      });
     }, (err) => {
-      console.log('Error: ', err);
+      console.log('Error: getData: ', err);
     });
   }
 
-  setPriceGraphData(volume, limit) {
+  getTrianglePattern() {
+
+  }
+
+  setPriceGraphData(price, pattern, limit) {
     this.price = new StockChart({
       rangeSelector: {
         selected: 1
-    },
-
-    title: {
+      },
+      title: {
         text: 'Dukascopy Historical Price'
-    },
-    plotOptions: {
-      series: {
-        turboThreshold: limit
-      }
-    },
-    series: [{
+      },
+      plotOptions: {
+        series: {
+          turboThreshold: limit
+        }
+      },
+      credits: {
+        enabled: false,
+      },
+      series: [{
         name: 'Price',
-        data: volume,
-        type: 'spline',
-    }]
+        data: price,
+        type: 'line',
+        color: '#734dc4',
+        id: 'price'
+      }, {
+        name: 'Pattern',
+        data: pattern,
+        type: 'line',
+        color: '#1fa47a',
+        id: 'pattern'
+      }],
     });
   }
+
+  // setPriceGraphData(volume, limit) {
+  //   this.price = new StockChart({
+  //     plotOptions: {
+  //       series: {
+  //         turboThreshold: limit
+  //       }
+  //     },
+  //     credits: {
+  //       enabled: false,
+  //     },
+  //     series: [{
+  //       // type: 'flags'
+  //       data: volume,
+  //       zones: [{
+  //         value: 119600.43,
+  //         color: '#f7a35c'
+  //       }, {
+  //         value: 196030.94,
+  //         color: '#7cb5ec'
+  //       }]
+  //     }],
+  //   });
+  // }
 
   setStockGraphData(stockdata, volume, limit) {
     this.stock = new StockChart({
@@ -113,7 +166,7 @@ export class StatisticsComponent implements OnInit {
         name: 'FOREX',
         data: stockdata,
       }, {
-        type: 'column',
+        type: 'area',
         name: 'Volume',
         data: volume,
         yAxis: 1,
