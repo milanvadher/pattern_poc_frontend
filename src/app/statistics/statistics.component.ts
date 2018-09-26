35fe54b4ee8d4cca25eb87eaa54ab952c8131f16
Milan Vadher <milanvadher1996@gmail.com>
2018-09-26 13:12:09 +0530

    add simple line chart

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -20,7 +20,6 @@ export class StatisticsComponent implements OnInit {
 
   constructor(public api: RestapiService) {
     this.getData();
-    this.getTrianglePattern();
   }
 
   ngOnInit() {
@@ -35,30 +34,55 @@ export class StatisticsComponent implements OnInit {
         this.stockData.push([new Date(data[0]['date'][index]).getTime(), data[1]['open'][index], data[2]['high'][index], data[3]['low'][index], data[4]['close'][index]]);
         this.volumeData.push([new Date(data[0]['date'][index]).getTime(), data[5]['volume'][index]]);
       }
-      this.api.getApi('/getTrianglePattern').subscribe((pattern: any) => {
-        console.log('Triangle Pattern: ', pattern.trianglePattern);
-        const trianglePattern = pattern.trianglePattern;
-        for (let index = 0; index < pattern.trianglePattern.length; index++) {
-          for (let i = 0; i < 3; i++) {
-            this.trianglePattern.push([new Date(trianglePattern[index][i][0]).getTime(), trianglePattern[index][i][1]]);
-          }
-        }
-        console.log(new Date(trianglePattern[25][0][0]).getTime());
-        this.setPriceGraphData(this.volumeData, this.trianglePattern, data[0]['date'].length);
         this.setStockGraphData(this.stockData, this.volumeData, data[0]['date'].length);
-      }, (err) => {
-        console.log('Error: getTrianglePattern: ', err);
-      });
+        this.setPriceGraphData(this.volumeData, data[0]['date'].length);
+
+      // this.api.getApi('/getTrianglePattern').subscribe((pattern: any) => {
+      //   console.log('Triangle Pattern: ', pattern.trianglePattern);
+      //   const trianglePattern = pattern.trianglePattern;
+      //   for (let index = 0; index < pattern.trianglePattern.length; index++) {
+      //     for (let i = 0; i < 3; i++) {
+      //       this.trianglePattern.push([new Date(trianglePattern[index][i][0]).getTime(), trianglePattern[index][i][1]]);
+      //     }
+      //   }
+      //   console.log(new Date(trianglePattern[25][0][0]).getTime());
+      //   this.setTrianglePtternData(this.volumeData, this.trianglePattern, data[0]['date'].length);
+      //   this.setStockGraphData(this.stockData, this.volumeData, data[0]['date'].length);
+      // }, (err) => {
+      //   console.log('Error: getTrianglePattern: ', err);
+      // });
     }, (err) => {
       console.log('Error: getData: ', err);
     });
   }
 
-  getTrianglePattern() {
-
+  setPriceGraphData(price, limit) {
+    this.price = new StockChart({
+      rangeSelector: {
+        selected: 1
+      },
+      title: {
+        text: 'Dukascopy Historical Price'
+      },
+      plotOptions: {
+        series: {
+          turboThreshold: limit
+        }
+      },
+      credits: {
+        enabled: false,
+      },
+      series: [{
+        name: 'Price',
+        data: price,
+        type: 'line',
+        color: '#734dc4',
+        id: 'price'
+      }],
+    });
   }
 
-  setPriceGraphData(price, pattern, limit) {
+  setTrianglePtternData(price, pattern, limit) {
     this.price = new StockChart({
       rangeSelector: {
         selected: 1
@@ -89,30 +113,6 @@ export class StatisticsComponent implements OnInit {
       }],
     });
   }
-
-  // setPriceGraphData(volume, limit) {
-  //   this.price = new StockChart({
-  //     plotOptions: {
-  //       series: {
-  //         turboThreshold: limit
-  //       }
-  //     },
-  //     credits: {
-  //       enabled: false,
-  //     },
-  //     series: [{
-  //       // type: 'flags'
-  //       data: volume,
-  //       zones: [{
-  //         value: 119600.43,
-  //         color: '#f7a35c'
-  //       }, {
-  //         value: 196030.94,
-  //         color: '#7cb5ec'
-  //       }]
-  //     }],
-  //   });
-  // }
 
   setStockGraphData(stockdata, volume, limit) {
     this.stock = new StockChart({
@@ -166,7 +166,7 @@ export class StatisticsComponent implements OnInit {
         name: 'FOREX',
         data: stockdata,
       }, {
-        type: 'area',
+        type: 'column',
         name: 'Volume',
         data: volume,
         yAxis: 1,
