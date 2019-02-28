43c71d9c172f8dde6bd86376b83b3736c9c59208
Milan Vadher <milanvadher1996@gmail.com>
2019-02-28 18:17:42 +0530

    add other prod key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_origin: 'http://18.237.88.115/:5000'
+  api_origin: 'http://18.237.88.115:5000',
+  otherLocal: 'http://18.237.88.115:3000'
 };
